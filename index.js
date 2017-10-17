fdf8c74710c36de60a9f599d69a7df4bfad35a1d
Max Spencer <mrmaxspencer@googlemail.com>
2017-10-17 23:35:03 +0100

    Refactor

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const request = require('request');
 
 exports.handler = (event, context, callback) => {
     request('https://uploadbeta.com/api/fortune/', function (error, response, body) {
-        callback(error, JSON.parse(body));
+	if (error) {
+	    callback(error);
+	} else {
+	    var fortune = JSON.parse(body);
+            callback(error, fortune);
+	}
     });
 };
