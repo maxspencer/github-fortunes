9805b646cb10af77811d67403aab29b5f6e96960
Max Spencer <mrmaxspencer@googlemail.com>
2017-10-17 23:38:14 +0100

    Use JWT

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const request = require('request');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 exports.handler = (event, context, callback) => {
     request('https://uploadbeta.com/api/fortune/', function (error, response, body) {
@@ -8,7 +10,9 @@ exports.handler = (event, context, callback) => {
 	    callback(error);
 	} else {
 	    var fortune = JSON.parse(body);
-            callback(error, fortune);
+	    var privateKey = fs.readFileSync('fortunes.private-key.pem');
+	    var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' });
+            callback(error, token);
 	}
     });
 };
