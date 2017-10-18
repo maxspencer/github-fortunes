9da6fa390d0183093afec9c29c1212b6529ca900
Max Spencer <mrmaxspencer@googlemail.com>
2017-10-18 08:41:53 +0100

    Add proper fields to JWT

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const request = require('request');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
+function getAuthToken() {
+    var privateKey = fs.readFileSync('fortunes.private-key.pem');
+    var payload = {
+	iss: 6023
+    };
+    var options = {
+	expiresIn: '1h',
+	algorithm: 'RS256'
+    };
+    return jwt.sign(payload, privateKey, options);
+}
+
 exports.handler = (event, context, callback) => {
+    console.log(JSON.stringify(event, null, 2));
     request('https://uploadbeta.com/api/fortune/', function (error, response, body) {
 	if (error) {
 	    callback(error);
 	} else {
 	    var fortune = JSON.parse(body);
-	    var privateKey = fs.readFileSync('fortunes.private-key.pem');
-	    var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' });
+	    var token = getAuthToken();
             callback(error, token);
 	}
     });
